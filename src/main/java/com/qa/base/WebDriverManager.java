cec444ac970e9fe7306153f7213f46dcca5cce6d
Snehasish <karak.snehasish@gmail.com>
2020-12-26 22:48:27 +0800

    added driver exe in resourceFolder

--- src/main/java/com/qa/base/WebDriverManager.java
+++ src/main/java/com/qa/base/WebDriverManager.java
@@ -5,24 +5,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.File;
 
 
 public class WebDriverManager {
 
 
-    public static WebDriver initializeWebDriver(){
-        WebDriver driver=null;
-        String browserName=TestBase.testBase.readProperties("Browser");
+    public static WebDriver initializeWebDriver() {
+        WebDriver driver = null;
+        String browserName = TestBase.testBase.readProperties("Browser");
 
-            if(browserName.equalsIgnoreCase("chrome")){
-                System.setProperty("webdriver.chrome.driver","C:\\Users\\sneha\\Downloads\\chromedriver_win32\\chromedriver.exe");
-                driver=new ChromeDriver();
+        if (browserName.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") +
+                    File.separator + "src" + File.separator + "test" +
+                    File.separator + "Resources" + File.separator + "chromedriver.exe");
+            driver = new ChromeDriver();
 
-            }
-            else if(browserName.equalsIgnoreCase("firefox")){
-                System.setProperty("webdriver.gecko.driver","D:\\Software\\geckodriver.exe");
-                driver=new FirefoxDriver();
-            }
-            return driver;
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") +
+                    File.separator + "src" + File.separator + "test" +
+                    File.separator + "Resources" + File.separator + "geckodriver.exe");
+
+            driver = new FirefoxDriver();
+        }
+        return driver;
     }
 }
